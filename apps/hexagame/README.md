c1b9f8660d9a7c02f17ad12dd5273d4cf3ed93a6
François Marie De Mey <eeddow@gmail.com>
2025-01-20 08:05:21 +0200

    multiple three.js

--- apps/hexagame/README.md
+++ apps/hexagame/README.md
@@ -1,4 +1,5 @@
 # TODO
 
-## Three
-- WARNING: Multiple instances of Three.js being imported.
+- InstancedMesh
+- puzzle infinite land
+- rivers
--- apps/hexagame/vite.config.ts
+++ apps/hexagame/vite.config.ts
@@ -1,12 +1,10 @@
 import { paraglide } from '@inlang/paraglide-sveltekit/vite'
-import svg from '@poppanator/sveltekit-svg'
 import { sveltekit } from '@sveltejs/kit/vite'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		svg(),
 		paraglide({
 			project: './project.inlang',
 			outdir: './src/lib/paraglide',
